983da182175e8789c458b773fbdd5a0d2da9d5a3
Gordun Gordey macbook <gordey.moskvichev@gmail.com>
2023-03-06 15:56:44 +0400

    request bot

--- api/webhook.js
+++ api/webhook.js
@@ -6,11 +6,32 @@ module.exports = async (request, response) => {
     try {
       const bot = new telegramBot (process.env.BOT_TOKEN, {polling:true});
 
-      bot.onText(/\/start/, async (msg) => {
-        const chatId = msg.chat.id;
+      const { body } = request;
+
+      // Ensure that this is a message being sent
+      if (body.message) {
+          // Retrieve the ID for this chat
+          // and the text that the user sent
+          const { chat: { id }, text } = body.message;
+
+          // Create a message to send back
+          // We can use Markdown inside this
+          const message = `✅ Thanks for your message: *"${text}"*\nHave a great day! 👋🏻`;
+
+          // Send our new message back in Markdown
+          await bot.sendMessage(id, message, {parse_mode: 'Markdown'});
+        bot.onText(/\/start/, async (msg) => {
+          const chatId = msg.chat.id;
+        
+          await bot.sendMessage(chatId, 'Здравствуйте! \n Это бот района Yacht Party');
+        });
+      }
+
+      // bot.onText(/\/start/, async (msg) => {
+      //   const chatId = msg.chat.id;
       
-        await bot.sendMessage(chatId, 'Здравствуйте! \n Это бот района Yacht Party');
-      });
+      //   await bot.sendMessage(chatId, 'Здравствуйте! \n Это бот района Yacht Party');
+      // });
     }
     catch(error) {
         // If there was an error sending our message then we 
