ec2f50ff5f9fad25fe6b27bf15ee429c09a0c322
Gordun Gordey macbook <gordey.moskvichev@gmail.com>
2023-03-06 15:41:52 +0400

    webhook main

--- api/webhook.js
+++ api/webhook.js
@@ -1,31 +1,31 @@
-// Require our Telegram helper package
-const TelegramBot = require('node-telegram-bot-api');
 
+const { connectBot } = require('../bot/index');
 // Export as an asynchronous function
 // We'll wait until we've responded to the user
 module.exports = async (request, response) => {
     try {
-        // Create our new bot handler with the token
-        // that the Botfather gave us
-        // Use an environment variable so we don't expose it in our code
-        const bot = new TelegramBot(process.env.BOT_TOKEN);
+        connectBot();
+        // // Create our new bot handler with the token
+        // // that the Botfather gave us
+        // // Use an environment variable so we don't expose it in our code
+        // const bot = new TelegramBot(process.env.BOT_TOKEN);
 
-        // Retrieve the POST request body that gets sent from Telegram
-        const { body } = request;
+        // // Retrieve the POST request body that gets sent from Telegram
+        // const { body } = request;
 
-        // Ensure that this is a message being sent
-        if (body.message) {
-            // Retrieve the ID for this chat
-            // and the text that the user sent
-            const { chat: { id }, text } = body.message;
+        // // Ensure that this is a message being sent
+        // if (body.message) {
+        //     // Retrieve the ID for this chat
+        //     // and the text that the user sent
+        //     const { chat: { id }, text } = body.message;
 
-            // Create a message to send back
-            // We can use Markdown inside this
-            const message = `✅ Thanks for your message: *"${text}"*\nHave a great day! 👋🏻`;
+        //     // Create a message to send back
+        //     // We can use Markdown inside this
+        //     const message = `✅ Thanks for your message: *"${text}"*\nHave a great day! 👋🏻`;
 
-            // Send our new message back in Markdown
-            await bot.sendMessage(id, message, {parse_mode: 'Markdown'});
-        }
+        //     // Send our new message back in Markdown
+        //     await bot.sendMessage(id, message, {parse_mode: 'Markdown'});
+        // }
     }
     catch(error) {
         // If there was an error sending our message then we 
