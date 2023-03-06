bde9d36b8d8a75d925a572ab411a9d3be966e9ff
Gordun Gordey macbook <gordey.moskvichev@gmail.com>
2023-03-06 19:36:08 +0400

    del switch

--- api/webhook.js
+++ api/webhook.js
@@ -29,26 +29,43 @@ module.exports = async (request, response) => {
         if (callback_query) {
           const { data, message } = callback_query;
           const { chat: { id }, text } = message;
-          switch (data) {
-            case CALLBACK_DATA.showEvent.callback_data:
-              const response = await fetch("https://yacht-backend.vercel.app/api/events/");
-              if (response.ok) {
-                const events = await response.json();
-                events.map(async (event) => {
-                    await bot.sendPhoto(
-                      id,
-                      'https://res.cloudinary.com/zoonyanya/image/upload/v1666294757/cld-sample-2.jpg',
-                      { caption: getEventMessage(event), reply_markup: getEventInlineKeyboard(event._id.toString(), isAdmin(id)) }
-                    )
-                  })
-              } else {
-                bot.sendMessage(id, 'Событие не найдены');
-              }
-             break;
-            case CALLBACK_DATA.yachts.callback_data:
-             return bot.sendMessage(id, 'Яхты яхты');
-             break;
+
+          if (data == CALLBACK_DATA.showEvent.callback_data) {
+            const response = await fetch("https://yacht-backend.vercel.app/api/events/");
+            if (response.ok) {
+              const events = await response.json();
+              events.map(async (event) => {
+                  await bot.sendPhoto(
+                    id,
+                    'https://res.cloudinary.com/zoonyanya/image/upload/v1666294757/cld-sample-2.jpg',
+                    { caption: getEventMessage(event), reply_markup: getEventInlineKeyboard(event._id.toString(), isAdmin(id)) }
+                  )
+                })
+                return ;
+            } else {
+              return bot.sendMessage(id, 'Событие не найдены');
+            }
           }
+          // switch (data) {
+          //   case CALLBACK_DATA.showEvent.callback_data:
+          //     const response = await fetch("https://yacht-backend.vercel.app/api/events/");
+          //     if (response.ok) {
+          //       const events = await response.json();
+          //       events.map(async (event) => {
+          //           await bot.sendPhoto(
+          //             id,
+          //             'https://res.cloudinary.com/zoonyanya/image/upload/v1666294757/cld-sample-2.jpg',
+          //             { caption: getEventMessage(event), reply_markup: getEventInlineKeyboard(event._id.toString(), isAdmin(id)) }
+          //           )
+          //         })
+          //     } else {
+          //       bot.sendMessage(id, 'Событие не найдены');
+          //     }
+          //    break;
+          //   case CALLBACK_DATA.yachts.callback_data:
+          //    return bot.sendMessage(id, 'Яхты яхты');
+          //    break;
+          // }
         }
     }
     catch(error) {
