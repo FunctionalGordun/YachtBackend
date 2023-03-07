21e9fd297534c8e3d02ad6aee93d2fed215bea09
Gordun Gordey macbook <gordey.moskvichev@gmail.com>
2023-03-07 16:32:35 +0400

    server bot

--- bot/index.js
+++ bot/index.js
@@ -20,9 +20,13 @@ const connectBot = async () => {
     
       await bot.sendMessage(chatId, 'Здравствуйте! \n Это бот района Yacht Party', {reply_markup: getMainInlineKeyboard()});
     });
+  
+    bot.onText(/\/admin/, async (msg) => {
+      const chatId = msg.chat.id;
+    
+      await  bot.sendMessage(chatId, `Возможности администратора`, { reply_markup: getAdminKeyboard()});
+    });
 
-
-let eventStatus = null
     bot.on('message', async (msg) => {
       const { chat, contact = null, text } = msg;
       const { id: chatId } = chat;
@@ -31,38 +35,9 @@ let eventStatus = null
         const { phone_number, first_name, last_name, user_id } = contact;
         bot.sendMessage(chatId, `Вы отправили свой телефон ${phone_number}`);
       }
-      // if (text == 'tete') {
-      //   bot.sendMessage(chatId, `TETETE ${eventStatus}`).then(res => {
-      //     console.log("RES", res)
-      //   })
-      //   eventStatus = 'tete'
-      // } else if (text == 'ooo') {
-      //   bot.sendMessage(chatId, `OOO ${eventStatus}`)
-      //   eventStatus = 'ooo'
-      // }
-
-      bot.sendMessage(chatId, `ID ${chatId}`);
-
       if (text == '/admin' && isAdmin(chatId)) {
         bot.sendMessage(chatId, `Возможности администратора`, { reply_markup: getAdminKeyboard()});
       }
-
-      // bot.sendMessage(chatId, `admin ${chatId}`);
-      // if (isAdmin(chatId)) {
-      //   bot.sendMessage(chatId, `Вы админ`);
-      //   bot.sendLocation(chatId, 25.091951, 55.141493)
-      //   bot.sendMessage(chatId, 'Бронируй нахуй', bookingKeyboard)
-      // }
-
-      // if(msg?.web_app_data?.data) {
-      //   try {
-      //       const data = JSON.parse(msg?.web_app_data?.data)
-      //       console.log(data)
-      //       await bot.sendMessage(chatId, 'Спасибо за обратную связь!')
-      //   } catch (e) {
-      //       console.log(e);
-      //   }
-      // }
     });
 
 
@@ -78,15 +53,17 @@ let eventStatus = null
               res.map(async (event) => {
                 await bot.sendPhoto(
                   chatId,
-                  'https://res.cloudinary.com/zoonyanya/image/upload/v1666294757/cld-sample-2.jpg',
-                  { caption: getEventMessage(event), reply_markup: getEventInlineKeyboard(event._id.toString(), isAdmin(chatId)) }
-                )
+                  event.image,
+                  { caption: getEventMessage(event), reply_markup: getEventInlineKeyboard(event._id.toString(), isAdmin(id), event?.address?.latitude, event?.address?.longitude)}
+                  )
               })
             }
-          }).catch(err => console.log('error', err))
+          }).catch(err => {
+            return bot.sendMessage(id, 'События не найдены');
+          })
          break;
-        case CALLBACK_DATA.yachts.callback_data:
-         return bot.sendMessage(chatId, 'Яхты яхты');
+        case 'location':
+         return bot.sendMessage(chatId, 'Локация');
          break;
       }
     })
@@ -94,13 +71,4 @@ let eventStatus = null
     console.log('BOT failed!', err.message);
   }
 }
-
-                // await bot.sendMediaGroup(
-                //   chatId,
-                //   [{"type": "photo", "media": 'https://res.cloudinary.com/zoonyanya/image/upload/v1666294757/cld-sample-2.jpg'},
-                //   {"type": "photo", "media": 'https://res.cloudinary.com/zoonyanya/image/upload/v1666294757/cld-sample-2.jpg'},
-                //   {"type": "photo", "media": 'https://res.cloudinary.com/zoonyanya/image/upload/v1666294757/cld-sample-2.jpg'}],
-                //   { caption: getEventMessage(event)
-                // })
-
 module.exports = { connectBot, bot };
